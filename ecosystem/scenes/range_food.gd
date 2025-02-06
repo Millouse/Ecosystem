c0513cec5d65fe9034e9d9f2fc892660a3a57188
Virbek <61600649+Virbek@users.noreply.github.com>
2025-02-06 11:57:43 +0100

    add food system

---
+++ ecosystem/scenes/range_food.gd
@@ -0,0 +1,32 @@
+extends Area3D
+
+var num_fruit : int = 0
+var its_time : float = 2.0
+var timer : float = 0.0
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	timer += delta
+	if timer >= its_time:
+		num_fruit += 1
+		timer = 0
+	pass
+
+
+func _on_body_entered(body: Node3D) -> void:
+	if body is CharacterBody3D :
+		if body.want_eat:
+			body.num_fruit += num_fruit
+			num_fruit = 0
+			body.want_eat = false
+			body.want_stock = true
+			print("fruit de la creature",body.num_fruit)
+			print("fruit de l'arbre", num_fruit)
+		pass
+		
+	print("Un objet est entré :", body.name)
+	pass # Replace with function body.
